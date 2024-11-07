6a6e3fb377b7cab893ce1787d7a2a4682cd28b80
JoseCanolaVas <jose.canola@sumimedica.com>
2024-11-07 16:45:08 -0500

    creacion de los modulos restantes con sus archivos sin configuraciones, se añadio el providers y el bases.

---
+++ app/Http/Modules/Equipos/Controllers/EquipoController.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Modules\Equipos\Controllers;
+
+use Illuminate\Database\Eloquent\Model;
+
+class EquipoController extends Model
+{
+    //
+}
---
+++ app/Http/Modules/Proyectos/models/Proyecto.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Http\Modules\Proyectos\Models;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Proyecto extends Model
+{
+    //
+}
