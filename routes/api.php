2e3ee5ed098188927d4cd9090019d51c54edd729
JoseCanolaVas <jose.canola@sumimedica.com>
2024-11-07 15:29:05 -0500

    se crean los modulos del proyecto y la rutas de los archivos por api , se crea los encarpetados en modules aun sin sus archivos.

---
+++ routes/api.php
@@ -0,0 +1,22 @@
+<?php
+
+// Modulo equipos
+require __DIR__ . '/equipos/equipos.php';
+
+//Modulo Proyectos
+require __DIR__ . '/proyectos/proyectos.php';
+
+//Modulo Tareas
+require __DIR__ . '/tareas/tareas.php';
+
+//Modulo Comentarios
+require __DIR__ . '/comentarios/comentarios.php';
+
+//Modulo Etiquetas
+require __DIR__ . '/etiquetas/etiquetas.php';
+
+//Modulo Categorias
+require __DIR__ . '/categorias/categorias.php';
+
+
+
